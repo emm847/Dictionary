34776e72d1a87667f507f75091c4f70b062ca426
EF-Code <kongkickster@gmail.com>
2024-12-26 21:15:25 +0100

    Added nested dictionary with two languages (words and english translation pairs)

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,21 @@
 #This program translates words in the specified languages to English
+
+
+# dictionary with some words and translations
+dictionary = {
+    "French": {
+        "bonjour": "good morning", "merci": "thank you", "au revoir": "goodbye",
+        "oui": "yes", "non": "no", "bon": "good", "grand": "big", "petit": "small",
+        "maison": "house", "chien": "dog", "chat": "cat", "vie": "life", "eau": "water",
+        "soleil": "sun", "lune": "moon", "viens": "come", "humaine": "human", "feu": "fire",
+        "traduire": "translate", "chirurgie": "surgery"
+    },
+    "Spanish": {
+        "hola": "hello", "gracias": "thank you", "adios": "goodbye", "si": "yes",
+        "no": "no", "casa": "house", "aqua": "water", "vida": "life", "sol": "sun",
+        "grande": "big", "bueno": "good", "malo": "bad", "perro": "dog", "amor": "love",
+        "gate": "cat", "feliz": "happy", "cielo": "heaven", "traducir": "translate", "fuego": "fire",
+        "humana": "human"
+    }
+
+}
